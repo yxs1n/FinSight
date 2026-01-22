e6f36f600cd51a0dd72bfc8ea8e277bc2abed408
yxs1n <yasin041121@gmail.com>
2026-01-22 22:33:09 +0000

    Database Connection update

--- config/db.js
+++ config/db.js
@@ -1,11 +1,16 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
+// Create a connection pool to the database
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
+// Export the pool for use in other modules
 module.exports = pool.promise();
